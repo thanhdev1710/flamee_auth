b4eb8077f862ddc8c994af7ee35a7535f1729777
thanhdev1710 <chithanh171004@gmail.com>
2025-11-20 15:27:58 +0700

    update

--- internal/routes/user/posts/interaction.router.go
+++ internal/routes/user/posts/interaction.router.go
@@ -10,18 +10,13 @@ import (
 type InteractionRouter struct{}
 
 func (ir *InteractionRouter) InitInteractionRouter(Router *gin.RouterGroup) {
-	// Public router
-	InteractionRouterPublic := Router.Group("/interactions")
-	{
-		InteractionRouterPublic.GET("/:postId", utils.ForwardTo(global.Url.UrlPostService))
-	}
 	// Private router
 	InteractionRouterPrivate := Router.Group("/interactions").
 		Use(middlewares.AuthMiddleware()).
 		Use(middlewares.VerifyAccount())
 	{
+		InteractionRouterPrivate.GET("/:postId", utils.ForwardTo(global.Url.UrlPostService))
 		InteractionRouterPrivate.POST("/like/:postId", utils.ForwardTo(global.Url.UrlPostService))
-		InteractionRouterPrivate.DELETE("/like/:postId", utils.ForwardTo(global.Url.UrlPostService))
 		InteractionRouterPrivate.POST("/comment/:postId", utils.ForwardTo(global.Url.UrlPostService))
 		InteractionRouterPrivate.DELETE("/comment/:postId", utils.ForwardTo(global.Url.UrlPostService))
 		InteractionRouterPrivate.POST("/share/:postId", utils.ForwardTo(global.Url.UrlPostService))
